a8f1f91b63c0a948bfe9f9b5805812499b44c48a
Mariusz Kurzyk <mariusz.kurzyk@gmail.com>
2018-04-07 11:07:28 +0200

    js added

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+console.log(allNames); 
+
+var newName = 'Marian';
+
+if (allNames.indexOf(newName) === -1) {
+var newName = allNames.push('Marian');
+console.log(allNames); 
+} else {
+console.log(allNames); 
+}
